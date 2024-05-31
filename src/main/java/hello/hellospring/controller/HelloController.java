5c9545735ae85f0cb85086be8303ac20c91721fa
hyejin <hyejda00@naver.com>
2024-06-01 02:55:58 +0900

    2024.05.31 RequestParam을 이용하여 파라미터 전달

---
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,42 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller//컨트롤러 사용시 붙는 어노테이션
+public class HelloController {
+
+    @GetMapping("hello") // /hello라고 들어오면 이 메소드를 호출
+    public String hello(Model model) { // 여기서 model은 MVC에서의 MODEL을 의미
+        model.addAttribute("data","Hello World");
+        return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model ) {// 외부에서 파라미터를 받을때는 @RequestParam 어노테이션 사용
+       // required 를 false로 하면 해당 파라미터 값이 필수로 안들어와도 된다, required의 기본값은 true
+        // 파라미터를 넘길 때 웹브라우저에서 ?{RequestParam이름}={값}
+        model.addAttribute("name",name); // 외부에서 받은 파라미터를 model에 담아서 전달
+        return "hello-template";
+    }
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
